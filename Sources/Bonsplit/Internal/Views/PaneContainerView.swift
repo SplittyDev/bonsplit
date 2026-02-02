b09929e140adf9236ee49b7d197a35a42fce7769
SplittyDev <splittydev@protonmail.com>
2026-02-03 00:48:16 +0700

    Remove pane background color

--- Sources/Bonsplit/Internal/Views/PaneContainerView.swift
+++ Sources/Bonsplit/Internal/Views/PaneContainerView.swift
@@ -53,7 +53,6 @@ struct PaneContainerView<Content: View, EmptyContent: View>: View {
             contentAreaWithDropZones
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(nsColor: .textBackgroundColor))
     }
 
     // MARK: - Content Area with Drop Zones
